85b829ba4e802117e7bd77b69f26f1d4fedd812f
Odohofre <odohofrebright@gmail.com>
2023-01-31 01:25:02 +0100

    finish the front-end

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/legacy/image'
+import { useState } from 'react'
 
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
 function BlurImage() {
+  const [isLoading, setLoading] = useState(true)
+
   return (
     <a href='#' className='group'>
       <div className='w-full aspect-w-1 aspect:h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
-        <Image alt="" layout='fill' objectFit='cover' src="https://bit.ly/placeholder-img" className='group-hover:opacity-75' />
+        <Image alt="" layout='fill' objectFit='cover' src="https://bit.ly/placeholder-img" className={cn('group-hover:opacity-75', isLoading ? 'grayscale blur-2xl slace-110': 'grayscale-0 blur-0 scale-100')} onLoadingComplete={() => setLoading(false)} />
       </div>
       <h3 className='mt-4 text-sm text-gray-700'> Lee Robinson</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
