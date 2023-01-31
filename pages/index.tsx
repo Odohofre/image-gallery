f103ed467e62fa29b72e501a1922922e6b611e19
Odohofre <odohofrebright@gmail.com>
2023-01-31 01:10:17 +0100

    update Image to BlurImage to avoid clas with image comp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 
 
-function Image() {
+function BlurImage() {
   return (
     <a href='#' className='group'>
       <div className='w-full aspect-w-1 aspect:h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
-        <img alt="" src="https://bit.ly/placeholder-img" className='group-hover:opacity-75' />
+        <Image alt="" layout='fill' objectFit='cover' src="https://bit.ly/placeholder-img" className='group-hover:opacity-75' />
       </div>
       <h3 className='mt-4 text-sm text-gray-700'> Lee Robinson</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
